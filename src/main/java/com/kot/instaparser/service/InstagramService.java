3404298d29b32bf28fc7b9fda5a03e72689d6151
Andrii Kotiuha <akotiuha@yukon.cv.ua>
2020-10-03 21:50:02 +0300

    added getCommonFollowers method

--- src/main/java/com/kot/instaparser/service/InstagramService.java
+++ src/main/java/com/kot/instaparser/service/InstagramService.java
@@ -8,7 +8,10 @@ import org.brunocvcunha.instagram4j.requests.payload.InstagramSearchUsernameResu
 import org.brunocvcunha.instagram4j.requests.payload.InstagramUserSummary;
 import org.springframework.stereotype.Service;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -50,8 +53,66 @@ public class InstagramService {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
+	public void getCommonFollowers(String firstAccount, String secondAccount) throws IOException {
+		List<String> firstFollowers = new ArrayList<>();
+		List<String> secondFollowers = new ArrayList<>();
+		FileWriter writer1 = new FileWriter("firstFollowers.txt");
+		FileWriter writer2 = new FileWriter("secondFollowers.txt");
+
+		InstagramSearchUsernameResult cheResult = loginService.doLogin().sendRequest(
+				new InstagramSearchUsernameRequest(firstAccount));
+
+		InstagramSearchUsernameResult karnaResult = loginService.doLogin().sendRequest(
+				new InstagramSearchUsernameRequest(secondAccount));
+
+		String nextMaxId = null;
+		while (true) {
+			InstagramGetUserFollowersResult fr = loginService.doLogin()
+					.sendRequest(new InstagramGetUserFollowersRequest(cheResult.getUser().getPk(), nextMaxId));
+			for(InstagramUserSummary user: fr.getUsers()){
+				firstFollowers.add(user.username);
+				writer1.write(user.username + System.lineSeparator());
+			}
+			nextMaxId = fr.getNext_max_id();
+			if (nextMaxId == null) {
+				break;
+			}
+		}
+		writer1.close();
+
+		while (true) {
+			InstagramGetUserFollowersResult fr = loginService.doLogin()
+					.sendRequest(new InstagramGetUserFollowersRequest(karnaResult.getUser().getPk(), nextMaxId));
+			for(InstagramUserSummary user: fr.getUsers()){
+				secondFollowers.add(user.username);
+				writer2.write(user.username + System.lineSeparator());
+			}
+			nextMaxId = fr.getNext_max_id();
+			if (nextMaxId == null) {
+				break;
+			}
+		}
+		writer2.close();
+
+		List<String> commonFollowers = new ArrayList<>();
+
+		firstFollowers.stream()
+				.filter(secondFollowers::contains)
+				.map(String::valueOf)
+				.sorted()
+				.forEach(s -> {
+					commonFollowers.add(s);
+					System.out.println(s);
+				});
+
+		FileWriter writer = new FileWriter("output.txt");
+		for(Object str: commonFollowers) {
+			writer.write(str + System.lineSeparator());
+		}
+
+		writer.close();
+	}
 
 }
