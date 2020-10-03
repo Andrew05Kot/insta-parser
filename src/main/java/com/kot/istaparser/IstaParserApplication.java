80d492cc5849d1253238e422a0bae78e443b317e
Andrii Kotiuha <akotiuha@yukon.cv.ua>
2020-10-03 17:32:35 +0300

    Initial project

---
+++ src/main/java/com/kot/istaparser/IstaParserApplication.java
@@ -0,0 +1,13 @@
+package com.kot.istaparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstaParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstaParserApplication.class, args);
+	}
+
+}
